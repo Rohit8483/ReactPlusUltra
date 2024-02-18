c64fdc6ba486b4d8f5698f24d6915ae15a6daa9c
rohit hirulkar <rohit.hirulkar@globallogic.com>
2024-02-18 18:56:23 +0530

    code for initial code setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = document.getElementById("root");
+const rootInstance = ReactDOM.createRoot(root);
+rootInstance.render(<App />);
